d76d2a484e40fe4f5e825e08651cda366ae25b58
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-12-01 04:05:16 -0600

    create folders for files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 const { uploadFile } = require("../helpers");
 
-loadFile = async (req, res = response) => {
+const loadFile = async (req, res = response) => {
 	if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
 		res.status(400).json({ msg: "No files to upload" });
 		return;
@@ -9,7 +9,13 @@ loadFile = async (req, res = response) => {
 
 	//Images
 	try {
-		const name = await uploadFile(req.files);
+		// const name = await uploadFile(
+		// 	req.files,
+		// 	["txt", "md", "docx"],
+		// 	"texts"
+		// );
+		const name = await uploadFile(req.files, undefined, "imgs");
+
 		res.json({
 			name,
 		});
