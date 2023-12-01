b78259b6d0c6b6f32ce2b79902569d7808493c55
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-12-01 03:52:52 -0600

    create Helper - uploadFile

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,36 +1,23 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-
 const { response } = require("express");
+const { uploadFile } = require("../helpers");
 
-loadFile = (req, res = response) => {
+loadFile = async (req, res = response) => {
 	if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
 		res.status(400).json({ msg: "No files to upload" });
 		return;
 	}
 
-	const { file } = req.files;
-	const nameCut = file.name.split(".");
-	const extension = nameCut[nameCut.length - 1];
-
-	//Validate extention
-	const validExtensions = ["png", "jpg", "jpeg", "gif"];
-	if (!validExtensions.includes(extension)) {
-		return res.status(400).json({
-			msg: `The extention ${extension} is not valid, must be ${validExtensions}`,
+	//Images
+	try {
+		const name = await uploadFile(req.files);
+		res.json({
+			name,
+		});
+	} catch (error) {
+		res.status(400).json({
+			msg: error,
 		});
 	}
-
-	const nameTemp = uuidv4() + "." + extension;
-	const uploadPath = path.join(__dirname, "../uploads/", nameTemp);
-
-	file.mv(uploadPath, (err) => {
-		if (err) {
-			return res.status(500).json({ err });
-		}
-
-		res.json({ msg: "File uploaded to " + uploadPath });
-	});
 };
 
 module.exports = {
