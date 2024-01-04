e6c291a0fa07916ae72fbf77b1f892dd33f54150
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-01-04 03:10:33 -0600

    return single string of the errors

--- middlewares/validate-fields.js
+++ middlewares/validate-fields.js
@@ -1,13 +1,17 @@
 const { validationResult } = require("express-validator");
 
-
-const validateFields = (req,res, next) => {
+const validateFields = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res.status(400).json(errors);
+		// console.log(errors.errors);
+		const errorMessages = errors.errors
+			.map((error) => error.msg)
+			.join(", ");
+		return res.status(400).json({ msg: errorMessages });
+		// return res.status(400).json(errors);
 	}
 
-  next();
+	next();
 };
 
 module.exports = {
